Atom-X-Devs/android_kernel_xiaomi_sm7325

sound: import XIOAMI changes
From lisa-r-oss

Change-Id: I28afa7b63a4712bd11a8cb7caab4e6942f77533e
Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

sound/soc/soc-pcm.c
@@ -38,6 +38,10 @@ static const struct snd_pcm_hardware no_host_hardware = {
 				  SNDRV_PCM_INFO_PAUSE |
 				  SNDRV_PCM_INFO_RESUME,
 	.formats		= SNDRV_PCM_FMTBIT_S16_LE |
+#ifdef COMFIG_MACH_XIAOMI
+				  SNDRV_PCM_FMTBIT_S24_LE |
+				  SNDRV_PCM_FMTBIT_S24_3LE |
+#endif
 				  SNDRV_PCM_FMTBIT_S32_LE,
 	.period_bytes_min	= PAGE_SIZE >> 2,
 	.period_bytes_max	= PAGE_SIZE >> 1,

baikalos/android_kernel_xiaomi_redwood

sound/soc/soc-pcm.c
@@ -38,6 +38,10 @@ static const struct snd_pcm_hardware no_host_hardware = {
 				  SNDRV_PCM_INFO_PAUSE |
 				  SNDRV_PCM_INFO_RESUME,
 	.formats		= SNDRV_PCM_FMTBIT_S16_LE |
+#ifdef CONFIG_MACH_XIAOMI
+				  SNDRV_PCM_FMTBIT_S24_LE |
+				  SNDRV_PCM_FMTBIT_S24_3LE |
+#endif
 				  SNDRV_PCM_FMTBIT_S32_LE,
 	.period_bytes_min	= PAGE_SIZE >> 2,
 	.period_bytes_max	= PAGE_SIZE >> 1,